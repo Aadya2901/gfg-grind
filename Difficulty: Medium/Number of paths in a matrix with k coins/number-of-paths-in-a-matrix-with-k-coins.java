04ad8570417abc47e5d546a489be8220af4a6cd8
Aadya2901 <145851414+Aadya2901@users.noreply.github.com>
2025-11-08 23:16:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfPath(int[][] mat, int k) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[][][] dp = new int[n][m][k + 1];
+
+        if (mat[0][0] <= k)
+            dp[0][0][mat[0][0]] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int sum = mat[i][j]; sum <= k; sum++) {
+
+                    if (i > 0 && sum - mat[i][j] >= 0)
+                        dp[i][j][sum] += dp[i - 1][j][sum - mat[i][j]];
+
+                    if (j > 0 && sum - mat[i][j] >= 0)
+                        dp[i][j][sum] += dp[i][j - 1][sum - mat[i][j]];
+                }
+            }
+        }
+        return dp[n - 1][m - 1][k];
+    }
+}

dbbda0ea251d19becd7398e4cc272aaa3bbedc94
Aadya2901 <145851414+Aadya2901@users.noreply.github.com>
2025-11-16 11:37:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Increasing Subsequence/longest-common-increasing-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int LCIS(int[] a, int[] b) {
+        int n = a.length, m = b.length;
+        int[] dp = new int[m];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currentMax = 0;  // best LCIS for values < a[i]
+
+            for (int j = 0; j < m; j++) {
+
+                if (a[i] > b[j]) {
+                    // update best possible LCIS ending before b[j]
+                    currentMax = Math.max(currentMax, dp[j]);
+                }
+
+                else if (a[i] == b[j]) {
+                    // extend LCIS
+                    dp[j] = currentMax + 1;
+                    ans = Math.max(ans, dp[j]);
+                }
+            }
+        }
+        return ans;
+    }
+}
